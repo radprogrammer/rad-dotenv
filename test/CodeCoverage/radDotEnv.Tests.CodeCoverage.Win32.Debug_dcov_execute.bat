f1dde30ce377069b894c0db9027256218bce0d88
Darian Miller <darian@darianmiller.com>
2024-09-30 18:42:56 -0500

    Code Coverage support added via the Delphi Code Coverage Wizard Plus https://github.com/MHumm/delphi-code-coverage-wizard-plus Note: To prevent local paths in the batch file, you need the Delphi Code Coverage binary within the system path
    All Tests Pass (71)
    Code Coverage for Win32\Debug = 80.6%

---
+++ test/CodeCoverage/radDotEnv.Tests.CodeCoverage.Win32.Debug_dcov_execute.bat
@@ -0,0 +1,1 @@
+ "CodeCoverage_x32.exe" -e "..\bin\radDotEnv.DUnitX.Tests.Win32.Debug.exe" -m "..\bin\radDotEnv.DUnitX.Tests.Win32.Debug.map" -sd "..\..\source\" -ife -uf "radDotEnv.Tests.CodeCoverage.Win32.Debug_dcov_units.lst" -spf "radDotEnv.Tests.CodeCoverage.Win32.Debug_dcov_paths.lst" -od "Reports\Win32\Debug" -v -html -efe
